ff8ea03c2a8b8278304262042da4db82e97084bd
Cedric van Putten <me@bycedric.com>
2020-10-20 14:15:08 +0200

    fix: add workaround for notifications in ssr

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 // @generated: @expo/next-adapter@2.1.0
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+      <Text>With this patch-package fix, we can load <b>expo-notifications</b>: {typeof Notifications}</Text>
     </View>
   );
 }
